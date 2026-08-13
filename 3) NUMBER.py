501789f85f3c2f3855d74762a699f198217df25c
sumitmidya09-tech <sumitmidya09@gmail.com>
2026-08-13 17:17:45 +0530

    Add files via upload

---
+++ 3) NUMBER.py
@@ -0,0 +1,68 @@
+A=12
+print("A=12")
+B=34
+print("B=34")
+c=A+B
+print("A+B=",c)
+
+A=3
+print("A=3")
+B=4
+print("B=4")
+c=A-B
+print("A-B=",c)
+
+A=3
+print("A=3")
+B=4
+print("B=4")
+c=A/B
+print("A/B=",c)
+
+A=4
+print("A=4")
+B=5
+print("B=5")
+c=A*B
+print("A*B=",c)
+
+A=11
+print("A=11")
+B=2
+print("B=2")
+c=A%B
+print("A%B=",c)
+
+A=3
+print("A=3")
+B=2
+print("B=2")
+c=A**B
+print("A**B=",c)
+
+
+
+
+nyc_bal=300
+bal_pitt=894
+total_distance=nyc_bal+bal_pitt
+print("nyc bal: ",nyc_bal)
+print("bal_pritt: ",bal_pitt)
+print("total distance is: ",total_distance)
+mph=65
+print("mph: ",mph)
+time=total_distance/mph
+print("time taken",time)
+
+
+a=round(time,2)
+print("round of: ",a)
+
+
+
+
+
+
+
+
+
