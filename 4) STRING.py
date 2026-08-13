07903ad90aa007cf66863fc4f0a5649e94a5cbd4
sumit midya <sumitmidya09@gmail.com>
2026-08-13 17:37:13 +0530

    Add files via upload

---
+++ 4) STRING.py
@@ -0,0 +1,40 @@
+text="ice cream"
+print(text)
+a=text[0]
+print(a)
+b=text[1]
+print(b)
+#text[0]='g'
+#str error will come
+c=text[0:3]
+print(c)
+d=text[4:9]
+print(d)
+e=text[4:]
+print(e)
+f=text[:4]
+print(f)
+text='helo'
+#text='let's lear python
+#syntax error will come
+text="let's learn python"
+print(text)
+text='helo "world"'
+print(text)
+#adders="1 purple street
+#error syntax then can not write next line by enter click
+adders='''1 purple street
+new york
+usa'''
+print(adders)
+a='hello'
+b='world'
+c=a+b
+print(c)
+#a="total states in usa'
+# error syntax
+a='total states in usa'
+num=25
+#c=a+num
+#print(c)
+#error type
