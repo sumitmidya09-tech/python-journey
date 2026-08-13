a2d69d02348d1f8449b3373cae98b1f09121697a
sumitmidya09-tech <sumitmidya09@gmail.com>
2026-08-13 16:52:40 +0530

    2) VARIABLE IN PYTHON

---
+++ variable in python.py
@@ -0,0 +1,11 @@
+house_reant=1000
+gas_rate=400.5
+groceries_rates=355.9
+print("house reant: ",house_reant)
+total=house_reant+gas_rate+groceries_rates
+print("total: ",total)
+reant =1500
+item_1="reant"
+item_2="gas"
+item_3="groceries"
+print("expense items: ",item_1,item_2,item_3)
