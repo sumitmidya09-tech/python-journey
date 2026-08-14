00648f21bc2b8e810850254008dfd88f52ae0285
sumit midya <sumitmidya09@gmail.com>
2026-08-14 12:04:41 +0530

    Add files via upload

---
+++ 12) IMPORT MODULES.py
@@ -0,0 +1,28 @@
+import math
+a=math.sqrt(16)
+print(a)
+b=math.pow(2,5)
+print(b)
+c=math.log10(100)
+print(c)
+d=math.log(1000)
+print(d)
+e=math.floor(2.6)
+print(e)
+f=math.ceil(2.4)
+print(f)
+
+
+
+#calendar
+
+import calendar
+cal=calendar.month(2016,9)
+print(cal)
+
+cal=calendar.isleap(2016)
+print(cal)
+cal=calendar.isleap(2017)
+print(cal)
+
+
